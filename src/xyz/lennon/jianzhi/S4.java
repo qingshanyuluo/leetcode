2e17e9b8dddfc94fe2a870374672c4aac09257e2
lennon <qingshanyuluo@gmail.com>
2019-11-08 19:41:32 +0800

    重建二叉树

---
+++ src/xyz/lennon/jianzhi/S4.java
@@ -0,0 +1,46 @@
+package xyz.lennon.jianzhi;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * S4
+ * 前序遍历{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}
+ */
+public class S4 {
+
+    public TreeNode reConstructBinaryTree(int [] pre,int [] in) {
+        TreeNode root=reConstructBinaryTree(pre,0,pre.length-1,in,0,in.length-1);
+        return root;
+    }
+    private TreeNode reConstructBinaryTree(int [] pre,int startPre,int endPre,int [] in,int startIn,int endIn) {
+         
+        if(startPre>endPre||startIn>endIn)
+            return null;
+        TreeNode root=new TreeNode(pre[startPre]);
+         
+        for(int i=startIn;i<=endIn;i++)
+            if(in[i]==pre[startPre]){
+                root.left=reConstructBinaryTree(pre,startPre+1,startPre+i-startIn,in,startIn,i-1);
+                root.right=reConstructBinaryTree(pre,i-startIn+startPre+1,endPre,in,i+1,endIn);
+                break;
+            }
+                 
+        return root;
+    }
+
+    public static void main(String[] args) {
+        S4 s4 = new S4();
+        int[] pre = {1,2,4,7,3,5,6,8};
+        int[] in = {4,7,2,1,5,3,8,6};
+        TreeNode root = s4.reConstructBinaryTree(pre, in);
+        System.out.println(root);
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
